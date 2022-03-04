53849607447a471cc194b76ee0aebcdb9934597b
simone celia <simone.celia@thinkopen.it>
2022-03-04 02:47:37 +0100

    create_an_user

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,6 +3,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +23,10 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return userDaoService.findOne(id);
+	}
+
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		User savedUser = userDaoService.save(user);
 	}
 }
