956277ec25a470850e5b38140e85b1947b4db59a
simone <simone.celia@csi.it>
2022-03-08 22:27:30 +0100

    static_filtering

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,8 +1,13 @@
 package com.in28minutes.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(value = {"field1"})
 public class SomeBean {
 	private String field1;
 	private String field2;
+	@JsonIgnore
 	private String field3;
 
 	public SomeBean(String field1, String field2, String field3) {
