50465998735b51c2283d37a2e67a9e4fb6a13c61
simone celia <simone.celia@thinkopen.it>
2022-03-10 21:01:17 +0100

    switch

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -55,4 +55,16 @@ public class UserJPAResource {
 	public void deleteUser(@PathVariable int id) {
 		userRepository.deleteById(id);
 	}
+
+	@GetMapping("/jpa/users/{id}/posts")
+	public List<Post> retrieveAllUsers(@PathVariable int id) {
+		Optional<User> userOptional = userRepository.findById(id);
+
+		if(!userOptional.isPresent()) {
+			throw new UserNotFoundException("id-" + id);
+		}
+
+		return userOptional.get().getPosts();
+
+	}
 }
